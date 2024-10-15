5677e283006c80dde6c926ce5bd4b5798d32fc88
Leticia Lima <leticia_lele.lima@hotmail.com>
2024-10-15 09:39:52 -0300

    subindo o projeto

---
+++ src/test/java/edu/leticia/streams_api/StreamsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.leticia.streams_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
